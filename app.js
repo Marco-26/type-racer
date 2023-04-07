871da607de649ebeef2e5cdf654682d2ff385cfb
Marco <markcostah@gmail.com>
2023-04-07 12:16:12 +0100

    adjustements

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-WORDS = []
-const numberOfSetences = 2;
+words = []
+const numberOfSetences = 1;
 const apiUrl = `https://baconipsum.com/api/?type=all-meat&paras=1&sentences=${numberOfSetences}&type=meat-and-filler`;
 
- fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    WORDS = data[0].split(' ');
-    showWords()
-  })
-  .catch(error => console.error(error))
+async function getData(){
+  try{
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    words = data[0].split(' ');
+    showWords(words);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
 
+getData();
 
 let currentWordIndex = 0;
 let currentLetterIndex = 0;
@@ -17,30 +22,29 @@ let currentLetterIndex = 0;
 let inputArea = document.getElementById("inputbox");
 inputArea.addEventListener("input", checkInput);
 
-function showWords(){ 
+function showWords(words){ 
   let wordShowcaseContainer = document.getElementById("word-showcase");
-  for (let i = 0; i < WORDS.length; i++) {
+  for (let i = 0; i < words.length; i++) {
     const newDiv = document.createElement("div");
     newDiv.setAttribute("class","word");
-    for (let k = 0; k < WORDS[i].length; k++) {
+    for (let k = 0; k < words[i].length; k++) {
       const newParagraph = document.createElement("p");
-      newParagraph.textContent = WORDS[i].charAt(k);
+      newParagraph.textContent = words[i].charAt(k);
       newDiv.append(newParagraph);
     }
     wordShowcaseContainer.append(newDiv)
   }
 }
 
-showWords();
-
 function checkInput(event){
   const lastLetter = getLastLetter(event);
 
   let showcaseWordContainer = document.getElementById("word-showcase").children;
+
   let showcaseWord = showcaseWordContainer[currentWordIndex];
   let showcaseLetter = showcaseWord.children;
-  let wordToGuess = WORDS[currentWordIndex];
-  let input = document.getElementById("inputbox");
+  let wordToGuess = words[currentWordIndex];
+  let input = document.getElementById("inputbox");;
   let curWord = input.value;
 
   //TODO: CTRL - BACKSPACE, faz nada
@@ -50,19 +54,19 @@ function checkInput(event){
       return;
     }
     currentLetterIndex--;
-    showcaseLetter[currentLetterIndex].style.color = "black";
+    showcaseLetter[currentLetterIndex].style.color = "#5C5E62";
     return;
   }
 
   if(lastLetter == wordToGuess.charAt(currentLetterIndex)){
-    showcaseLetter[currentLetterIndex].style.color = "green";
+    showcaseLetter[currentLetterIndex].style.color = "#D1D0C5";
     currentLetterIndex++;
   }
   else{
     if(currentLetterIndex >= wordToGuess.length){
       return;
     }
-    showcaseLetter[currentLetterIndex].style.color = "red";
+    showcaseLetter[currentLetterIndex].style.color = "#CA4754";
     currentLetterIndex++;
   }
 
