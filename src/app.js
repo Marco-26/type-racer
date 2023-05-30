0b5cd18d6ad2a2b2ae96e45d77bbab9958236625
Marco <markcostah@gmail.com>
2023-05-30 14:33:59 +0100

    fix bug at endgame

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-//TODO: Obrigatório usar espaço para completar as palavras
 //TODO: Botão de reset funcional
 //TODO: Barra para escolher quanto tempo para escrever
+//TODO: A ultima palavra do array não deve precisar de espaço para acabar o jogo
 
 let words = [];
 let timerStarted = false;
 let wordsWritten = 0;
 let currentWordIndex = 0;
+let wordsTyped = 0;
 let currentLetterIndex = 0;
 let time = 30;
 let timeLeft = 30;
@@ -19,6 +20,7 @@ const endGameScreen = document.querySelector('.end');
 
 inputArea.addEventListener('input', checkInput);
 timer.innerText = timeLeft;
+console.log(timeLeft);
 
 gameScreen.style.display = 'block';
 endGameScreen.style.display = 'none';
@@ -29,10 +31,25 @@ function getRandomQuote() {
     .then((data) => data.content);
 }
 
-async function renderWords() {
+async function getFormattedWords(){
+  let wordsArr = [];
   let quote = await getRandomQuote();
-  words = quote.split(' ');
-  words = words.map(word => word + ' ');
+  wordsArr = quote.split(' ');
+  wordsArr = wordsArr.map((word, index) =>{
+    if(index == wordsArr.length - 1){
+      return word;
+    }
+    else{
+      return word + ' ';
+    }
+  });
+  console.log(wordsArr);
+  return wordsArr;
+}
+
+async function renderWords() {
+  words = await getFormattedWords();
+  console.log(words);
   let wordShowcaseContainer = document.getElementById('word-showcase');
   wordShowcaseContainer.innerHTML = '';
   for (let i = 0; i < words.length; i++) {
@@ -61,6 +78,7 @@ function checkInput(event) {
   let wordToGuess = words[currentWordIndex];
   let input = document.getElementById('inputbox');
   let curWord = input.value;
+  let totalWords = words.length;
 
   //TODO: CTRL - BACKSPACE, faz nada
 
@@ -88,15 +106,16 @@ function checkInput(event) {
     currentLetterIndex++;
   }
 
-  console.log(event.inputType == 'insertText' && event.data === ' ');
-
-  if (curWord == wordToGuess && (event.inputType == 'insertText' && event.data === ' ')) {
-    input.value = '';
-    currentWordIndex++;
-    currentLetterIndex = 0;
-    curWord = null;
-    wordsWritten++;
-    if (currentWordIndex >= words.length) {
+  if (curWord == wordToGuess) {
+    wordsTyped++;
+    if(wordsTyped == totalWords || event.inputType == 'insertText' && event.data === ' '){
+      input.value = '';
+      currentWordIndex++;
+      currentLetterIndex = 0;
+      curWord = null;
+      wordsWritten++;
+    }
+    if (currentWordIndex >= totalWords) {
       endGame();
     }
   }
@@ -108,7 +127,7 @@ function getLastLetter(event) {
 }
 
 async function endGame() {
-  await import('../endgame.js');
+  await import('./endgame.js');
   let wpm = (wordsWritten / time) * 60;
   localStorage.setItem('wpm', wpm);
   localStorage.setItem('charsTyped', currentLetterIndex);
--- src/endgame.js
+++ src/endgame.js
@@ -5,6 +5,22 @@ const wpmFormatted = parseInt(wpmStr);
 
 const wpmNumberContainer = document.querySelector('.wpm-number');
 const charsTypedContainer = document.querySelector('.chars-typed');
+const restartButton = document.querySelector('.restart-button');
+
+const gameScreen = document.querySelector('.game');
+const endGameScreen = document.querySelector('.end');
 
 wpmNumberContainer.innerText = wpmFormatted;
 charsTypedContainer.innerText = charsTyped;
+
+restartButton.addEventListener('click', () =>{
+  console.log('Clicked');
+  endGame();
+});
+
+async function endGame() {
+  //TODO: reset timer
+  await import('./app.js');
+  gameScreen.style.display = 'block';
+  endGameScreen.style.display = 'none';
+}
